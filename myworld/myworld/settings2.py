3f24fbf04a7c7eb36ece663e105c8b8cb6a432bd
Marti <s>
2022-04-24 13:22:44 +0200

    dat

---
+++ myworld/myworld/settings2.py
@@ -0,0 +1,15 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ChipoXD$test',
+        'USER': 'ChipoXD',
+        'PASSWORD': '9Dy6KAv6!Mmx57t',
+        'HOST': 'ChipoXD.mysql.pythonanywhere-services.com',
+    }
+}
